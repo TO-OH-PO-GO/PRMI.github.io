b0fbc09f9a16206fcd394a9014f9d1fc0592fadc
tim <klasskoch@gmail.com>
2024-08-28 04:31:57 +0900

    hello8

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const navList = document.querySelector('.nav-list');
 mobileMenu.addEventListener('click', () => {
     navList.classList.toggle('active');
     
-    if (navList.classList.contains('active')) {
+    
         
-        setTimeout(() => {
-            navList.classList.remove('active');
-        }, 2000); 
-    }
+    setTimeout(() => {
+        navList.classList.remove('active');
+    }, 2000); 
+    
 });
