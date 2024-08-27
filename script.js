db6a635dcbde1ede2ac071cbcbd0a54a615a2ead
tim <klasskoch@gmail.com>
2024-08-28 04:29:44 +0900

    hello7

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ const navList = document.querySelector('.nav-list');
 
 mobileMenu.addEventListener('click', () => {
     navList.classList.toggle('active');
+    
+    if (navList.classList.contains('active')) {
+        
+        setTimeout(() => {
+            navList.classList.remove('active');
+        }, 2000); 
+    }
 });
